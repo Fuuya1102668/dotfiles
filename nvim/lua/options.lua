e7519cf7f7a6e12ffe7de82069c2d31fe3817256
Fuuya1102668 <c1102668@planet.kanazawa-it.ac.jp>
2024-09-09 01:22:00 +0900

    nwe file

---
+++ nvim/lua/options.lua
@@ -0,0 +1,30 @@
+-- 行番号の表示
+vim.opt.number = true
+
+-- signcolumnを常に表示
+vim.opt.signcolumn = "yes"
+
+-- タブ幅の設定
+vim.opt.tabstop = 4
+vim.opt.expandtab = true
+
+-- ファイルを読み込むとき，タブの幅をスペース4つ分にする
+vim.opt.shiftwidth = 4
+
+-- 改行のとき自動でインデント
+vim.opt.smartindent = true
+
+-- カーソルのある行の色を変える
+vim.opt.cursorline = true
+
+-- 検索している文字に色をつける
+vim.opt.hlsearch = true
+
+-- jjでinsertからnormalへ
+vim.api.nvim_set_keymap('i', 'jj', '<ESC>', { noremap = true, silent = true })
+
+-- init.lua
+-- vim.keymap.set('n', '<leader>e', ':Neotree toggle<CR>', { noremap = true, silent = true })
+
+vim.opt.encoding = 'utf-8'
+vim.opt.fileencoding = 'utf-8'
