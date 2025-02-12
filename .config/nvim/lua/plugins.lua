4a5430ee89f1f106c85dcc176e01e73f9bc01d9e
Fuuya1102668 <c1102668@planet.kanazawa-it.ac.jp>
2025-02-12 19:09:01 +0900

    lazygitを追加

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -172,6 +172,15 @@ require("lazy").setup({
         end,
     },
     {
+        "kdheepak/lazygit.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim"
+        },
+        config = function()
+            vim.keymap.set("n", "<leader>lg", ":LazyGit<CR>", { noremap = true, silent = true })
+        end
+    },
+    {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
